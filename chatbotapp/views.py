a1ace0b196ba1e44d3f6cb1fd00dcee2bc34179d
park chanhyuk <pgg6713@gmail.com>
2022-01-21 14:38:44 +0900

    [add] mocking for chaSeDae

--- chatbotapp/views.py
+++ chatbotapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from .models import *
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .kakaojsonformat.response import insert_text, make_reply, insert_replies
 from .form import *
+import json
 
 # Create your views here.
 def menu_list(request):
@@ -17,3 +20,25 @@ def menu_create(request):
         form = MenuForm()
     ctx = {'form': form}
     return render(request, template_name='menu_form.html', context=ctx)
+
+
+def get_chaSeDae(request):
+    answer = request.body.decode('utf-8')
+    return_json_str = json.loads(answer)
+    return_str = return_json_str['userRequest']['utterance']
+
+    if return_str == "차세대융합기술원":
+        # 여기에 데이터 베이스에서 차세대 융합기술원에서 하루 전체 메뉴가져오는 로직 지금은 text 로 dummy 로 쓰겠음
+        text = "아침은 이거 점심은 이거 저녁은 이거 입니다."
+        answer = insert_text(text)
+
+        # 실제 보여줄 음식에 대한 메뉴는 위에서 처리했다 이 밑에는 이제 사용자의 클릭을 유도하는 메뉴 생성
+
+        reply = make_reply("월","월요일차세대융합기술원")
+        answer = insert_replies(answer,reply)
+
+        reply = make_reply("화","화요일차세대융합기술원")
+        answer = insert_replies(answer,reply)
+
+        return JsonResponse(answer)
+
--- yoongBab/urls.py
+++ yoongBab/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from chatbotapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chatbotapp.urls')),
+    path('chaSeDae/', views.get_chaSeDae),
 ]
