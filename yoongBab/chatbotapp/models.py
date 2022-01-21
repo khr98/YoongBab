3c4e21561396e1773f07bb1c9d63152c19fa3d63
park chanhyuk <pgg6713@gmail.com>
2022-01-21 11:18:04 +0900

    [fix] add nullable to model

--- yoongBab/chatbotapp/models.py
+++ yoongBab/chatbotapp/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # Create your models here.
 class ChaSeDae(models.Model):
-    moms = models.TextField(verbose_name="맘스")
-    chef = models.TextField(verbose_name="셰프")
-    special = models.TextField(verbose_name="정찬")
-    salad = models.TextField(verbose_name="샐러드")
-    dinner = models.TextField(verbose_name="석식")
-    takeOut = models.TextField(verbose_name="TakeOut")
+    moms = models.TextField(verbose_name="맘스",null=True)
+    chef = models.TextField(verbose_name="셰프",null=True)
+    special = models.TextField(verbose_name="정찬",null=True)
+    salad = models.TextField(verbose_name="샐러드",null=True)
+    dinner = models.TextField(verbose_name="석식",null=True)
+    takeOut = models.TextField(verbose_name="TakeOut",null=True)
