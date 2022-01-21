80eb55cf8ec93442e386687c5beae682a0fd1b01
park chanhyuk <pgg6713@gmail.com>
2022-01-21 15:29:08 +0900

    [test] change url

--- yoongBab/urls.py
+++ yoongBab/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from chatbotapp import views
 
 urlpatterns = [
+    path('asd/', views.get_chaSeDae),
     path('admin/', admin.site.urls),
     path('', include('chatbotapp.urls')),
 ]
